feaeb9f08f32764c61c3ba63f0bcc269f15589bd
sexerk <horordai@gmail.com>
2026-03-04 20:24:21 +0300

    fix3: fix migration

--- alembic/versions/afa84680f3da_add_amenities_and_room_amenity.py
+++ alembic/versions/afa84680f3da_add_amenities_and_room_amenity.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'afa84680f3da'
-down_revision: Union[str, Sequence[str], None] = 'b6ab69916aac'
+down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
